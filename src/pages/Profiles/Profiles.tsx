5cf7dbd297f9975b0612559ee5e6d30c8edd98a9
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-02-26 17:14:38 -0500

    map profiles

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -1,6 +1,9 @@
 // types
 import { Profile } from '../../types/models'
 
+//components
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
+
 
 interface ProfilesProps {
   profiles: Profile[];
@@ -11,13 +14,13 @@ const Profiles = (props: ProfilesProps): JSX.Element => {
   if(!profiles.length) return <p>No profiles yet</p>
 
   return (
-    <>
-      <h1>Hello. This is a list of all the profiles.</h1>
+    
+      <main className='list'>
       {profiles.map((profile: Profile) =>
-        <p key={profile.id}>{profile.name}</p>
+        <ProfileCard key={profile.id} profile={profile}/>
       )}
-    </>
+    </main>
   )
 }
- 
+
 export default Profiles
