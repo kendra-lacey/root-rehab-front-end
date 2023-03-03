4df96ac5d0e76d1df42442e65c79bec76a8b4ffe
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-03-02 22:38:18 -0500

    stub up HealthManager Component

---
+++ src/components/HealthManager/HealthManager.tsx
@@ -0,0 +1,27 @@
+//assets
+import heart from '../../assets/icons/heart.png'
+import noheart from '../../assets/icons/noheart.png'
+//types
+import { Plant } from '../../types/models'
+
+
+
+interface HealthManagerProps {
+  plant: Plant
+}
+
+const HealthManager = (props: HealthManagerProps): JSX.Element => {
+const { plant } = props
+
+
+
+
+  return ( 
+    <section>
+
+    </section>
+
+  );
+}
+
+export default HealthManager ;
